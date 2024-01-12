15c7f8b53c142fbc9a0d7fbe8fcde29619f09ebf
tomarkhushi <142433715+tomarkhushi@users.noreply.github.com>
2024-01-12 20:18:35 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+function formvalid() {
+    var vaildpass = document.getElementById("pass").value;
+  
+    if (vaildpass.length <= 8 || vaildpass.length >= 20) {
+      document.getElementById("vaild-pass").innerHTML = "Minimum 8 characters";
+      return false;
+    } else {
+      document.getElementById("vaild-pass").innerHTML = "";
+    }
+  }function show() {
+    var x = document.getElementById("pass");
+    if (x.type === "password") {
+      x.type = "text";
+      document.getElementById("showing").src =
+        "https://static.thenounproject.com/png/777494-200.png";
+    } else {
+      x.type = "password";
+      document.getElementById("showing").src =
+        "https://cdn2.iconfinder.com/data/icons/basic-ui-interface-v-2/32/hide-512.png";
+    }
+  }
